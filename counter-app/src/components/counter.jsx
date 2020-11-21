7694ed8b841238a72673c4edb89d5a595f217907
jana <rathan.kparam@gmail.com>
2020-11-21 22:04:49 +0800

    Set attributes

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    state = {
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
+    };
+
+    styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <br/>
+                <img src={this.state.imageUrl} alt=""/>
+                <br/>
+                <span style={this.styles} className='badge badge-primary m-3'>{this.formatCount()}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
+            </React.Fragment>
+        );
+    }
+
+    formatCount() {
+        const {count} = this.state; // object destructuring
+        return count === 0 ? 'Zero' : count ;
+    }
+
+}
+
+export default Counter;
