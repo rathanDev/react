ced9320640ebe60bc5c3d22e5e95e383d9fea38c
jana <rathan.kparam@gmail.com>
2020-12-29 02:37:17 +0800

    Init react login

---
+++ react-login/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from "./components/login";
+import TopNav from "./components/topNav";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-login/src/components/topNav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TopNav extends Component {
+
+    render() {
+
+        return (
+            <div>
+                TopNav
+            </div>
+        )
+    }
+}
+
+export default TopNav;
