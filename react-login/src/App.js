1457575cfb2f564ff556e349858667e7d923f4c2
jana <rathan.kparam@gmail.com>
2020-12-29 02:49:04 +0800

    Add top nav bar

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -4,9 +4,24 @@ import TopNav from "./components/topNav";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
+      <script src="https://cdn.plot.ly/plotly-latest.min.js" />
+
+      {/* <title>React-Title</title> */}
+
       <TopNav />
       <Login />
+
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
+        crossorigin="anonymous"></script>
+
     </div>
   );
 }
--- react-login/src/components/topNav.jsx
+++ react-login/src/components/topNav.jsx
@@ -6,7 +6,27 @@ class TopNav extends Component {
 
         return (
             <div>
-                TopNav
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+                    <a class="navbar-brand" href="{% url 'home' %}">
+                        <span class="navbar-brand"> &nbsp; Brand</span>
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                        aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <div class="my-0 ml-auto">
+                            <span class="mx-2 navbar-text">Hello User</span>
+                            <span><a class="btn btn-sm btn-secondary mx-2" href="{% url 'dashboard' %}">Dashboard</a></span>
+                            <span><a class="btn btn-sm btn-secondary mx-2" href="{% url 'logout' %}">Logout</a></span>
+                            <span> <a class="btn btn-sm btn-secondary mx-2" href="{% url 'login' %}">Login</a></span>
+                        </div>
+                    </div>
+
+                </nav>
+
             </div>
         )
     }
