86d61d71cd43db3b4cdda07838c69e45ebb254b9
jana <rathan.kparam@gmail.com>
2020-12-29 23:01:48 +0800

    Add login form

--- react-login/src/components/login.jsx
+++ react-login/src/components/login.jsx
@@ -4,12 +4,46 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                Login
+            <div id="top-container">
+
+                <div id="top-bg"></div>
+
+                <div class="container pb-5 pt-2">
+
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-4">
+                            <div class="shadow p-3 bg-white rounded" id="login-form">
+
+                                <h5> Login </h5>
+                                <hr />
+
+                                <form method="post" action="">
+
+
+                                    <label>Username</label>
+                                    <input type="text" name="username"/>
+
+                                    <label>Password</label>
+                                    <input type="password" name="password1"/>
+
+                                    <input type="submit" value="Login" className="btn btn-primary btn-sm btn-block" />
+                                
+                                    <div class="container errorMessage"></div>
+
+                                    <hr />
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         )
     }
-    
+
 }
 
 export default Login;
