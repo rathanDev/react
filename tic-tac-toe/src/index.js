c112153c9937fef555024eb12f66970b4b23d387
jana <rathan.kparam@gmail.com>
2021-02-18 22:30:36 +0800

    Store state in Board componnent

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -5,20 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -33,10 +26,17 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
     return (
       <Square
         value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
@@ -48,19 +48,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)} {this.renderSquare(1)} {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)} {this.renderSquare(4)} {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)} {this.renderSquare(7)} {this.renderSquare(8)}
         </div>
       </div>
     );
