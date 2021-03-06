5afc9bd04d8cd2c6fe26a33539045cf4672d9c61
jana <rathan.kparam@gmail.com>
2021-03-06 20:17:44 +0800

    Use arrow funcs

--- todo-deved/src/App.js
+++ todo-deved/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import './App.css';
+import Form from './components/form';
+import TodoList from './components/todoList';
+
+const App = () => {
+
+  const [inputText, setInputText] = useState('');
+
   return (
     <div className="App">
       <header>
-        <h1>Hello, DOers</h1>
+        <h1>Hello, toDOers</h1>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
+
 }
 
 export default App;
