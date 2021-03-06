de4153a065ae785908f6b0dc3c596f801a9d5da8
jana <rathan.kparam@gmail.com>
2021-03-06 13:20:03 +0800

    Learn redux basics

--- redux-deved/src/index.js
+++ redux-deved/src/index.js
@@ -4,6 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+
+// Store -> Globalized state
+
+// Action -> Increment
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+// Display it on the console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +49,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
