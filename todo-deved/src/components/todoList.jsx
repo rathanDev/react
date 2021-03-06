19c9732fd74168af236dde32ff7de7041fd21729
jana <rathan.kparam@gmail.com>
2021-03-06 21:02:33 +0800

    Add todos

--- todo-deved/src/components/todoList.jsx
+++ todo-deved/src/components/todoList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Todo from './todo';
 
 const TodoList = () => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                <Todo />
+                <Todo />
+                <Todo />
+            </ul>
         </div>
     )
 }
