ff20ddd523877a8b61d3d04fd53e1cb12b0ef062
jana <rathan.kparam@gmail.com>
2021-03-18 07:08:07 +0800

    Upload file

--- amplify-root-app3/src/App.js
+++ amplify-root-app3/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify, { Storage } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+class App extends Component {
+
+  state = { fileUrl: '', file: '', filename: '' };
+
+  handleChange = e => {
+    const file = e.target.files[0];
+    this.setState({
+      fileUrl: URL.createObjectURL(file),
+      file,
+      filename: file.name
+    })
+  }
+
+  saveFile = () => {
+    Storage.put(this.state.filename, this.state.file)
+      .then(() => {
+        console.log('Successfully saved file')
+        this.setState({ fileUrl: '', file: '', filename: '' })
+      })
+      .catch(err => {
+        console.log('Err uploading file')
+      })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        Amplify File App with React 
+        <hr />
+        <input type='file' onChange={this.handleChange} />
+        <img src={this.state.fileUrl} />
+        <button onClick={this.saveFile}>Save File</button>
+      </div>
+    );
+  }
+
+
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
