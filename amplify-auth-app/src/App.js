9dbf2fe14dd4a395cc2c4ea79d0e6c9dea19f0ac
jana <rathan.kparam@gmail.com>
2021-03-18 11:46:32 +0800

    Add router

--- amplify-auth-app/src/App.js
+++ amplify-auth-app/src/App.js
@@ -1,73 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify, { Storage } from 'aws-amplify';
 
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
 
-  const [fileName, setFileName] = useState('')
-  const [fileUrl, setFileUrl] = useState('')
-  const [file, setFile] = useState('')
-
-  useEffect(() => {
-    console.log('Inside useEffect similar to componentDidMount')
-
-    Storage.list('', { level: 'private' })
-      .then(data => {
-        console.log('list private ', data)
-      }).catch(err => { console.err('Err at list', err) })
-
-    Storage.get('fitness.png', { level: 'private', expires: 300 })
-      .then(data => {
-        console.log('FileUrl', data)
-        setFileUrl(data)
-      }).catch(err => { console.err('Err', err) })
-
-    // async
-    // const downloadUrl = await Storage.get('fitness.png', { level: 'private', expires: 300 });
-    // window.location.href = downloadUrl
-
-  })
-
-  const handleChange = (e) => {
-    const file = e.target.files[0];
-    setFile(file)
-    setFileName(file.name)
-    setFileUrl(URL.createObjectURL(file))
-  }
-
-  const saveFile = () => {
-    console.log('SaveFile ', fileName, file)
-    Storage.put(fileName, file, {
-      level: 'private',
-      contentType: 'image/jpeg'
-    }).then(() => {
-      console.log('Successfully saved file')
-      setFile('')
-      setFileName('')
-      setFileUrl('')
-    }).catch(err => {
-      console.log('Err uploading file')
-    })
-  }
-
   return (
-    <div className="App">
-      Amplify Auth App
-      <hr />
-      <input type='file' onChange={(e) => handleChange(e)} />
-      <br />
-      <button onClick={saveFile}>Save File</button>
-      <br />
-      <h3>File</h3>
-      <img src={fileUrl} />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
+
+  // return (
+  //   <div className="App">
+  //     Amplify Auth App
+  //     <AmplifySignOut />
+  //     <hr />
+  //     <input type='file' onChange={(e) => handleChange(e)} />
+  //     <br />
+  //     <button onClick={saveFile}>Save File</button>
+  //     <br />
+  //     <h3>File</h3>
+  //     <img src={fileUrl} />
+  //   </div>
+  // )
 
 
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default App;
+// export default withAuthenticator(App, { includeGreetings: true });
---
+++ amplify-auth-app/src/components/Dashboard.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './../aws-exports';
+Amplify.configure(awsconfig);
+
+const Dashboard = () => {
+
+    // const [fileName, setFileName] = useState('')
+    // const [fileUrl, setFileUrl] = useState('')
+    // const [file, setFile] = useState('')
+
+    // useEffect(() => {
+    //     console.log('Inside useEffect similar to componentDidMount')
+
+    //     Storage.list('', { level: 'private' })
+    //         .then(data => {
+    //             console.log('list private ', data)
+    //         }).catch(err => { console.error('Err at list', err) })
+
+    //     Storage.get('fitness.png', { level: 'private', expires: 300 })
+    //         .then(data => {
+    //             console.log('FileUrl', data)
+    //             if (data)
+    //                 setFileUrl(data)
+    //         }).catch(err => { console.error('Err', err) })
+
+    //     // async
+    //     // const downloadUrl = await Storage.get('fitness.png', { level: 'private', expires: 300 });
+    //     // window.location.href = downloadUrl
+
+    // })
+
+    // const handleChange = (e) => {
+    //     const file = e.target.files[0];
+    //     setFile(file)
+    //     setFileName(file.name)
+    //     setFileUrl(URL.createObjectURL(file))
+    // }
+
+    // const saveFile = () => {
+    //     console.log('SaveFile ', fileName, file)
+    //     Storage.put(fileName, file, {
+    //         level: 'private',
+    //         contentType: 'image/jpeg'
+    //     }).then(() => {
+    //         console.log('Successfully saved file')
+    //         setFile('')
+    //         setFileName('')
+    //         setFileUrl('')
+    //     }).catch(err => {
+    //         console.error('Err uploading file')
+    //     })
+    // }
+
+
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
+// export default withAuthenticator(Dashboard);
