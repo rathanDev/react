18813152433da5cebf2c66d5fdf4e7ddf4eea039
jana <rathan.kparam@gmail.com>
2023-04-25 20:44:16 +0800

    Select account

--- nft/src/App.js
+++ nft/src/App.js
@@ -4,21 +4,24 @@ import react, { useState, useEffect } from "react";
 // require("ethers")
 
 const App = () => {
+  const [selectedAddress, setSelectedAddress] = useState("");
+
   useEffect(() => {
-    connectToMetamask();
+    // connectToMetamask();
   }, []);
 
   const connectToMetamask = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     console.log("provider", provider);
+    const accounts = await provider.send("eth_requestAccounts", []);
+    console.log("accounts", accounts);
+    setSelectedAddress(accounts[0]);
   };
 
   return (
     <div>
       Learning EtherJs
-      <button onClick={connectToMetamask}>
-        Connect to Metamask
-      </button>
+      <button onClick={connectToMetamask}>Connect to Metamask</button>
     </div>
   );
 };
