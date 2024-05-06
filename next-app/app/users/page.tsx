3ef3671c68cddbc8e47ee07f0286f00bfa9701a6
Jana <rathan.kparam@gmail.com>
2024-05-06 19:26:35 +0800

    Configure cache

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -7,16 +7,20 @@ interface Post {
 
 const UserPage = async () => {
 
-    const res = await fetch('https://jsonplaceholder.typicode.com/typicode/demo/posts');
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/typicode/demo/posts',
+        // {cache: 'no-store'}
+        {next: { revalidate : 10 }}
+    );
     const posts: Post[] = await res.json();
     console.log('posts', posts);
 
     return (
         <>
-            <h1>Posts *</h1>
+            <h1>Posts</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
             <ul>
                 {posts.map(post => <li key={post.id}>{post.title}</li>)}
-                {/* {posts.map(e => <li key={e.id}>e.title</li>)} */}
             </ul>
         </>
     )
