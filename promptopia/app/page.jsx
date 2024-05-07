16791992886aa5410df6f43754d9695ff01fa3f3
Jana <rathan.kparam@gmail.com>
2024-05-08 01:08:14 +0800

    Use Link from next/link

---
+++ promptopia/app/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Feed from '@components/feed';
+import Nav from '@components/nav';
+import Provider from '@components/provider';
+
+const Home = () => {
+    return (
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>
+                Discover & Share
+                {/* <br className='max-md:hidden'/> */}
+                <br className='max-md'/>
+                <span className='orange_gradient text-center'>
+                    AI-powered prompts
+                </span>
+            </h1>
+            <p className='desc text-center'>
+                Promptia is an open-source AI prompting tool 
+            </p>
+
+            <Feed/>
+
+        </section>
+    )
+}
+
+export default Home;
