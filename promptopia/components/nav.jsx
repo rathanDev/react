5d89cd0d9a2311f782c3923d705cc499ccf4c384
Jana <rathan.kparam@gmail.com>
2024-05-08 18:54:47 +0800

    Add isUserLoggedIn section

--- promptopia/components/nav.jsx
+++ promptopia/components/nav.jsx
@@ -8,6 +8,9 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 
 const Nav = () => {
+
+    const isUserLoggedIn = true;
+
     return (
         <nav className='flex-between w-full mb-16 pt-3'>
             <Link href='/' className='flex gap-2 flex-center'>
@@ -20,6 +23,28 @@ const Nav = () => {
                 />
                 <p className='logo_text'>Promptopia</p>
             </Link>
+
+            <div className='sm:flex hidden'>
+                {
+                isUserLoggedIn 
+                ? (
+                    <div className='flex gap-3 md:gap-5'>
+                        <Link href='/create-prompt' className='black_btn'>Create post</Link>
+                        <button type='button' onClick={signOut} className='outline_btn'>Sign out</button>
+                        <Link href='/profile'>
+                            <Image 
+                            src='/assets/images/logo.svg'
+                            width={37}
+                            height={37}
+                            className='rounded-full'
+                            alt='profile'
+                            />
+                        </Link>
+                    </div>
+                    )
+                : (<></>)
+                }
+            </div>
 
         </nav>
     )
